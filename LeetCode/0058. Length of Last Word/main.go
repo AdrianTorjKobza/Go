b09d677389df8dfe3f2342c4d7a0fcf43438d7c2
Adrian Torj-Kobza <42949599+AdrianTorjKobza@users.noreply.github.com>
2023-05-29 20:24:17 +0300

    Add files via upload

---
+++ LeetCode/0058. Length of Last Word/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func lengthOfLastWord(s string) int {
+	count := 0
+
+	for i := len(s) - 1; i >= 0; i-- {
+		if s[i] == ' ' {
+			if count > 0 {
+				return count
+			}
+		} else {
+			count++
+		}
+	}
+
+	return count
+}
+
+func main() {
+	stringInput := "   fly me   to   the moon  "
+
+	wordLength := lengthOfLastWord(stringInput)
+	fmt.Println("Length of the last word: ", wordLength)
+}
